d16be049aec167fca01518fd9d64a8ee959c6ebc
Ajeet Kumar Jha <kumarjhaajeet653@gmail.com>
2023-10-28 02:40:17 -0700

    cancle function added

--- src/App.js
+++ src/App.js
@@ -6,33 +6,54 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [retrying, setRetrying] = useState(false);
+
   useEffect(() => {
-      fetchMoviesHandeler();
-  }, [])
-  function fetchMoviesHandeler() {
+    fetchMoviesHandler();
+  }, []);
+
+  function fetchMoviesHandler() {
     setIsLoading(true);
     fetch('https://swapi.dev/api/films')
-    .then(response => {
-       return response.json();
-    })
-    .then(data => {
-     const transformedMoives = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-     }) 
-     setMovies(transformedMoives);
-     setIsLoading(false);
-    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        const transformedMovies = data.results.map(movieData => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.log('Fetching Error...', error);
+        setIsLoading(false);
+        if (retrying) {
+          setTimeout(fetchMoviesHandler, 5000);
+        }
+      });
+  }
+
+  function handleRetry() {
+    setRetrying(true);
+    fetchMoviesHandler();
+  }
+
+  function handleCancelRetry() {
+    setRetrying(false);
   }
 
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandeler}>Fetch Movies</button>
+        <button onClick={retrying ? handleCancelRetry : handleRetry}>
+          {retrying ? 'Cancel Retry' : 'Retry Fetch'}
+        </button>
       </section>
       <section>
         {isLoading ? (
